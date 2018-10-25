66c894de271b2ad416e1cde53ec8ce4312ed3778
Yauheni Pakala <Yauheni.Pakala@softeq.com>
2018-10-25 16:40:54 +0300

    Add humanize datetimeoffset

--- Softeq.XToolkit.Chat/Services/FormatService.cs
+++ Softeq.XToolkit.Chat/Services/FormatService.cs
@@ -16,5 +16,19 @@ namespace Softeq.XToolkit.Chat.Services
         }
 
         public string ToShortTimeFormat(DateTime? dateTime) => dateTime?.ToString(ShortTimeFormat) ?? string.Empty;
+
+        public string Humanize(DateTimeOffset date, string today, string yesterday)
+        {
+            if (date.Date == DateTime.Today)
+            {
+                return today;
+            }
+            if (date.Date == DateTime.Today.AddDays(-1))
+            {
+                return yesterday;
+            }
+            var format = date.Year == DateTime.Today.Year ? "dd MMM" : "dd MMM yyyy";
+            return date.ToString(format);
+        }
     }
 }
