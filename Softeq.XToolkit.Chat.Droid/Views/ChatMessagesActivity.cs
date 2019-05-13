dc864c28a6e4e15e4efad975611ccd6cf29a9451
Siarhei Leushunou <siarhei.leushunou@softeq.com>
2019-05-13 19:18:23 +0300

    move droid chat input from activity

--- Softeq.XToolkit.Chat.Droid/Views/ChatMessagesActivity.cs
+++ Softeq.XToolkit.Chat.Droid/Views/ChatMessagesActivity.cs
@@ -32,23 +32,12 @@ namespace Softeq.XToolkit.Chat.Droid.Views
         private NavigationBarView _navigationBarView;
         private RecyclerView _conversationsRecyclerView;
         private ConversationsObservableRecyclerViewAdapter _conversationsAdapter;
-        private EditText _messageEditText;
-        private ImageButton _takeAttachmentButton;
-        private ImageButton _addAttachmentButton;
-        private ImageButton _sendButton;
-        private RelativeLayout _editingMessageLayout;
-        private TextView _editingMessageBodyTextView;
-        private ImageButton _editingMessageCloseButton;
         private ImageButton _scrollDownImageButton;
         private ContextMenuComponent _contextMenuComponent;
+        private ChatInputView _chatInputView;
         //private bool _shouldSendStateMessageToChat;
         private bool _isAdapterSourceInitialized;
         private bool _isAutoScrollToFooterEnabled = true;
-        private ImagePicker _imagePicker;
-        private View _editImageContainer;
-        private ImageButton _removeImageButton;
-        private ImageViewAsync _imagePreview;
-        private string _previewImageKey;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -69,50 +58,19 @@ namespace Softeq.XToolkit.Chat.Droid.Views
 
             _conversationsRecyclerView = FindViewById<RecyclerView>(Resource.Id.rv_conversations_list);
 
-            _messageEditText = FindViewById<EditText>(Resource.Id.et_conversations_message);
-            _messageEditText.Hint = ViewModel.MessageInput.EnterMessagePlaceholderString;
-
-            _editingMessageLayout = FindViewById<RelativeLayout>(Resource.Id.rl_conversations_editing_message);
-            _editingMessageBodyTextView = FindViewById<TextView>(Resource.Id.tv_editing_message_body);
-            _editingMessageCloseButton = FindViewById<ImageButton>(Resource.Id.ib_conversations_editing_message_close);
-            _editingMessageCloseButton.SetImageResource(StyleHelper.Style.EditingCloseButtonIcon);
-
             _scrollDownImageButton = FindViewById<ImageButton>(Resource.Id.ib_conversations_scroll_down);
             _scrollDownImageButton.SetImageResource(StyleHelper.Style.ScrollDownButtonIcon);
 
-            _takeAttachmentButton = FindViewById<ImageButton>(Resource.Id.ib_conversations_take_attachment);
-            _takeAttachmentButton.SetImageResource(StyleHelper.Style.TakeAttachmentButtonIcon);
-            _takeAttachmentButton.SetCommand(new RelayCommand(TakePhoto));
-
-            _addAttachmentButton = FindViewById<ImageButton>(Resource.Id.ib_conversations_add_attachment);
-            _addAttachmentButton.SetImageResource(StyleHelper.Style.AddAttachmentButtonIcon);
-            _addAttachmentButton.SetCommand(new RelayCommand(AddPhoto));
-
-            _sendButton = FindViewById<ImageButton>(Resource.Id.ib_conversations_send);
-            _sendButton.SetImageResource(StyleHelper.Style.SendMessageButtonIcon);
-            _sendButton.SetCommand(nameof(_sendButton.Click), new RelayCommand(Send));
+            _chatInputView = FindViewById<ChatInputView>(Resource.Id.ll_chat_input_view);
 
             InitializeConversationsRecyclerView();
 
             _contextMenuComponent = new ContextMenuComponent(ViewModel.MessageCommandActions);
 
-            _editingMessageCloseButton.SetCommand(nameof(_editingMessageCloseButton.Click), ViewModel.MessageInput.CancelEditingCommand);
             _scrollDownImageButton.SetCommand(nameof(_scrollDownImageButton.Click), new RelayCommand(() =>
             {
                 ScrollToPosition(_conversationsRecyclerView.GetAdapter().ItemCount - 1);
             }));
-
-            _imagePicker = new ImagePicker(Dependencies.PermissionsManager, Dependencies.IocContainer.Resolve<IImagePickerService>());
-
-            _editImageContainer = FindViewById<View>(Resource.Id.activity_chat_conversations_image_preview_container);
-            _imagePreview = FindViewById<ImageViewAsync>(Resource.Id.activity_chat_conversations_preview_image);
-            _removeImageButton = FindViewById<ImageButton>(Resource.Id.activity_chat_conversations_remove_image_button);
-            _removeImageButton.SetImageResource(StyleHelper.Style.RemoveImageButtonIcon);
-            _removeImageButton.SetCommand(new RelayCommand(RemoveAttachment));
-            _editImageContainer.Visibility = ViewStates.Gone;
-
-            var editingMessageHeader = FindViewById<TextView>(Resource.Id.tv_conversations_editing_message_header);
-            editingMessageHeader.Text = ViewModel.MessageInput.EditMessageHeaderString;
         }
 
         protected override void OnPause()
@@ -121,8 +79,7 @@ namespace Softeq.XToolkit.Chat.Droid.Views
             //{
             //    Messenger.Default.Send(new ChatInBackgroundMessage());
             //}
-
-            KeyboardService.HideSoftKeyboard(_messageEditText);
+            _chatInputView.HideKeyboard();
 
             base.OnPause();
         }
@@ -169,22 +126,6 @@ namespace Softeq.XToolkit.Chat.Droid.Views
                     _navigationBarView.SetTitle(ViewModel.ConnectionStatus.ConnectionStatusText);
                 });
             }));
-            Bindings.Add(this.SetBinding(() => ViewModel.MessageInput.MessageBody, () => _messageEditText.Text, BindingMode.TwoWay));
-            Bindings.Add(this.SetBinding(() => ViewModel.MessageInput.IsInEditMessageMode).WhenSourceChanges(() =>
-            {
-                if (ViewModel.MessageInput.IsInEditMessageMode)
-                {
-                    _editingMessageBodyTextView.Text = ViewModel.MessageInput.EditedMessageOriginalBody;
-
-                    KeyboardService.ShowSoftKeyboard(_messageEditText);
-                }
-                else
-                {
-                    KeyboardService.HideSoftKeyboard(_messageEditText);
-                }
-
-                _editingMessageLayout.Visibility = BoolToViewStateConverter.ConvertGone(ViewModel.MessageInput.IsInEditMessageMode);
-            }));
             Bindings.Add(this.SetBinding(() => ViewModel.MessagesList.Messages).WhenSourceChanges(() =>
             {
                 if (_isAdapterSourceInitialized)
@@ -207,16 +148,14 @@ namespace Softeq.XToolkit.Chat.Droid.Views
                 _isAdapterSourceInitialized = true;
             }));
 
-            Bindings.Add(this.SetBinding(() => _imagePicker.ViewModel.ImageCacheKey).WhenSourceChanges(() =>
-            {
-                if (_imagePicker.ViewModel.ImageCacheKey == null)
-                {
-                    CloseEditImageContainer();
-                    return;
-                }
+            _chatInputView.BindViewModel(ViewModel.MessageInput);
+        }
 
-                OpenEditImageContainer();
-            }));
+        protected override void DoDetachBindings()
+        {
+            base.DoDetachBindings();
+
+            _chatInputView.UnbindViewModel();
         }
 
         private void ScrollToPosition(int lastPosition)
@@ -238,7 +177,7 @@ namespace Softeq.XToolkit.Chat.Droid.Views
             _conversationsRecyclerView.AddOnScrollListener(
                 new ScrollDownScrollListener(new RelayCommand<bool>(ScrollDownButtonVisibilityHandler)));
             _conversationsRecyclerView.SetOnTouchListener(
-                new RecyclerViewTouchListener(new RelayCommand(() => KeyboardService.HideSoftKeyboard(_messageEditText))));
+                new RecyclerViewTouchListener(new RelayCommand(() => _chatInputView.HideKeyboard())));
         }
 
         private void ScrollDownButtonVisibilityHandler(bool isVisible)
@@ -268,61 +207,6 @@ namespace Softeq.XToolkit.Chat.Droid.Views
                     _conversationsRecyclerView.ScrollToPosition(scrollPosition);
                 });
             }
-        }
-
-        private void TakePhoto()
-        {
-            _imagePicker.OpenCamera();
-        }
-
-        private void AddPhoto()
-        {
-            _imagePicker.OpenGallery();
-        }
-
-        private void OpenEditImageContainer()
-        {
-            Execute.BeginOnUIThread(() =>
-            {
-                var key = _imagePicker.ViewModel.ImageCacheKey;
-                if (key == _previewImageKey)
-                {
-                    return;
-                }
-
-                _editImageContainer.Visibility = ViewStates.Visible;
-
-                _previewImageKey = key;
-
-                ImageService.Instance
-                    .LoadFile(key)
-                    .DownSampleInDip(60, 60)
-                    .IntoAsync(_imagePreview);
-            });
-        }
-
-        private void CloseEditImageContainer()
-        {
-            Execute.BeginOnUIThread(() =>
-            {
-                _editImageContainer.Visibility = ViewStates.Gone;
-                _previewImageKey = null;
-                _imagePicker.ViewModel.ImageCacheKey = null;
-                _imagePreview.SetImageDrawable(null);
-            });
-        }
-
-        private void RemoveAttachment()
-        {
-            _imagePicker.ViewModel.ImageCacheKey = null;
-            _previewImageKey = null;
-        }
-
-        private void Send()
-        {
-            var args = _imagePicker.GetPickerData();
-            ViewModel.MessageInput.SendMessageCommand.Execute(new GenericEventArgs<ImagePickerArgs>(args));
-            CloseEditImageContainer();
         }
     }
 }
