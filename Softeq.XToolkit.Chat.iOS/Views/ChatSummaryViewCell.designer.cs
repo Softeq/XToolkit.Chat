ababd5cc857d29667d629915f9cb9963d09a4885
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2019-05-24 18:59:36 +0300

    Fix after merge

--- Softeq.XToolkit.Chat.iOS/Views/ChatSummaryViewCell.designer.cs
+++ Softeq.XToolkit.Chat.iOS/Views/ChatSummaryViewCell.designer.cs
@@ -9,141 +9,128 @@ using System.CodeDom.Compiler;
 
 namespace Softeq.XToolkit.Chat.iOS.Views
 {
-	[Register ("ChatSummaryViewCell")]
-	partial class ChatSummaryViewCell
-	{
-		[Outlet]
-		UIKit.UILabel ChatNameLabel { get; set; }
-
-		[Outlet]
-		UIKit.UILabel DateTimeLabel { get; set; }
-
-		[Outlet]
-		UIKit.UIImageView LastMessageBodyPhotoIcon { get; set; }
-
-		[Outlet]
-		UIKit.UIImageView LastMessageBodyPhotoIcon { get; set; }
-
-		[Outlet]
-		UIKit.UILabel LastMessageBodyPhotoLabel { get; set; }
-
-
-		[Outlet]
-		UIKit.UILabel LastMessageBodyPhotoLabel { get; set; }
-
-
-		[Outlet]
-		UIKit.UIView LastMessageBodyPhotoView { get; set; }
-
-		[Outlet]
-		UIKit.UIView LastMessageBodyPhotoView { get; set; }
-
-		[Outlet]
-		UIKit.UILabel MessageBodyLabel { get; set; }
-
-		[Outlet]
-		UIKit.UIView ReadUnreadIndicator { get; set; }
-
-		[Outlet]
-		FFImageLoading.Cross.MvxCachedImageView SenderPhotoImageView { get; set; }
-
-		[Outlet]
-		UIKit.UIView UnreadMessageCountBackground { get; set; }
-
-		[Outlet]
-		UIKit.UILabel UnreadMessageCountLabel { get; set; }
-
-		[Outlet]
-		UIKit.UIView UnreadView { get; set; }
-
-		[Outlet]
-		UIKit.UILabel UsernameLabel { get; set; }
-		
+    [Register("ChatSummaryViewCell")]
+    partial class ChatSummaryViewCell
+    {
         [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
+        UIKit.UILabel ChatNameLabel { get; set; }
+
+        [Outlet]
+        UIKit.UILabel DateTimeLabel { get; set; }
+
+        [Outlet]
+        UIKit.UIImageView LastMessageBodyPhotoIcon { get; set; }
+
+        [Outlet]
+        UIKit.UILabel LastMessageBodyPhotoLabel { get; set; }
+
+        [Outlet]
+        UIKit.UIView LastMessageBodyPhotoView { get; set; }
+
+        [Outlet]
+        UIKit.UILabel MessageBodyLabel { get; set; }
+
+        [Outlet]
+        UIKit.UIView ReadUnreadIndicator { get; set; }
+
+        [Outlet]
+        FFImageLoading.Cross.MvxCachedImageView SenderPhotoImageView { get; set; }
+
+        [Outlet]
+        UIKit.UIView UnreadMessageCountBackground { get; set; }
+
+        [Outlet]
+        UIKit.UILabel UnreadMessageCountLabel { get; set; }
+
+        [Outlet]
+        UIKit.UIView UnreadView { get; set; }
+
+        [Outlet]
+        UIKit.UILabel UsernameLabel { get; set; }
+
+        [Outlet]
+        [GeneratedCode("iOS Designer", "1.0")]
         UIKit.UIStackView BodyContainer { get; set; }
 
-		void ReleaseDesignerOutlets ()
-		{
-            if (BodyContainer != null) {
-                BodyContainer.Dispose ();
+        void ReleaseDesignerOutlets()
+        {
+            if (BodyContainer != null)
+            {
+                BodyContainer.Dispose();
                 BodyContainer = null;
             }
 
-			if (ChatNameLabel != null) {
-				ChatNameLabel.Dispose ();
-				ChatNameLabel = null;
-			}
-
-			if (DateTimeLabel != null) {
-				DateTimeLabel.Dispose ();
-				DateTimeLabel = null;
+            if (ChatNameLabel != null)
+            {
+                ChatNameLabel.Dispose();
+                ChatNameLabel = null;
             }
 
-            if (LastMessageBodyPhotoIcon != null) {
-                LastMessageBodyPhotoIcon.Dispose ();
+            if (DateTimeLabel != null)
+            {
+                DateTimeLabel.Dispose();
+                DateTimeLabel = null;
+            }
+
+            if (MessageBodyLabel != null)
+            {
+                MessageBodyLabel.Dispose();
+                MessageBodyLabel = null;
+            }
+
+            if (ReadUnreadIndicator != null)
+            {
+                ReadUnreadIndicator.Dispose();
+                ReadUnreadIndicator = null;
+            }
+
+            if (SenderPhotoImageView != null)
+            {
+                SenderPhotoImageView.Dispose();
+                SenderPhotoImageView = null;
+            }
+
+            if (UnreadMessageCountBackground != null)
+            {
+                UnreadMessageCountBackground.Dispose();
+                UnreadMessageCountBackground = null;
+            }
+
+            if (UnreadMessageCountLabel != null)
+            {
+                UnreadMessageCountLabel.Dispose();
+                UnreadMessageCountLabel = null;
+            }
+
+            if (UnreadView != null)
+            {
+                UnreadView.Dispose();
+                UnreadView = null;
+            }
+
+            if (UsernameLabel != null)
+            {
+                UsernameLabel.Dispose();
+                UsernameLabel = null;
+            }
+
+            if (LastMessageBodyPhotoView != null)
+            {
+                LastMessageBodyPhotoView.Dispose();
+                LastMessageBodyPhotoView = null;
+            }
+
+            if (LastMessageBodyPhotoIcon != null)
+            {
+                LastMessageBodyPhotoIcon.Dispose();
                 LastMessageBodyPhotoIcon = null;
             }
 
-            if (LastMessageBodyPhotoLabel != null) {
-                LastMessageBodyPhotoLabel.Dispose ();
+            if (LastMessageBodyPhotoLabel != null)
+            {
+                LastMessageBodyPhotoLabel.Dispose();
                 LastMessageBodyPhotoLabel = null;
             }
-
-            if (LastMessageBodyPhotoView != null) {
-                LastMessageBodyPhotoView.Dispose ();
-                LastMessageBodyPhotoView = null;
-			}
-
-			if (MessageBodyLabel != null) {
-				MessageBodyLabel.Dispose ();
-				MessageBodyLabel = null;
-			}
-
-			if (ReadUnreadIndicator != null) {
-				ReadUnreadIndicator.Dispose ();
-				ReadUnreadIndicator = null;
-			}
-
-			if (SenderPhotoImageView != null) {
-				SenderPhotoImageView.Dispose ();
-				SenderPhotoImageView = null;
-			}
-
-			if (UnreadMessageCountBackground != null) {
-				UnreadMessageCountBackground.Dispose ();
-				UnreadMessageCountBackground = null;
-			}
-
-			if (UnreadMessageCountLabel != null) {
-				UnreadMessageCountLabel.Dispose ();
-				UnreadMessageCountLabel = null;
-			}
-
-			if (UnreadView != null) {
-				UnreadView.Dispose ();
-				UnreadView = null;
-			}
-
-			if (UsernameLabel != null) {
-				UsernameLabel.Dispose ();
-				UsernameLabel = null;
-			}
-
-			if (LastMessageBodyPhotoView != null) {
-				LastMessageBodyPhotoView.Dispose ();
-				LastMessageBodyPhotoView = null;
-			}
-
-			if (LastMessageBodyPhotoIcon != null) {
-				LastMessageBodyPhotoIcon.Dispose ();
-				LastMessageBodyPhotoIcon = null;
-			}
-
-			if (LastMessageBodyPhotoLabel != null) {
-				LastMessageBodyPhotoLabel.Dispose ();
-				LastMessageBodyPhotoLabel = null;
-			}
-		}
-	}
+        }
+    }
 }
